b069bcc00c4e99cb149ef00e623c8c1df73e926e
Ajay_Chintada <chintadaajay72645@gmail.com>
2026-03-27 22:08:23 +0530

    chore: add Playwright setup file Playwright/playwright.config.ts

---
+++ Playwright/playwright.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  outputDir: "./test-results",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 2 : undefined,
+  reporter: [
+    ["html", { outputFolder: "./playwright-report", open: "never" }],
+    ["json", { outputFile: "./test-results/results.json" }],
+    ["github"],
+    ["list"],
+  ],
+  use: {
+    baseURL: process.env.BASE_URL || "http://localhost:3000",
+    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+});
