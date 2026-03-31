1f7319d81649e680b28d14602910d0f02cd6f4ef
AjayChintada <ajayc@proximabiz.com>
2026-03-31 12:21:19 +0530

    Add correct login page Playwright test

---
+++ Playwright/tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('login feature', async ({ page }) => {
+  await page.goto('/login');
+
+  const usernameInput = page.locator('#username');
+  const passwordInput = page.locator('#password');
+  const submitButton = page.locator('button[type="submit"]');
+
+  await expect(usernameInput).toBeVisible();
+  await expect(passwordInput).toBeVisible();
+  await expect(submitButton).toBeVisible();
+  await expect(submitButton).toHaveText('Sign In');
+  await expect(page.getByText('RecipeFinder')).toBeVisible();
+});
